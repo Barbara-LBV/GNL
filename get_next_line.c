81dfbc2fecdb76e210028a5fd6a8ef815bffc4c8
Barbara LEFEBVRE <blefebvr@e1r6p3.clusters.42paris.fr>
2022-07-20 14:06:55 +0200

    update du 20 07 2022

--- get_next_line.c
+++ get_next_line.c
@@ -6,30 +6,29 @@
 /*   By: blefebvr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 09:32:13 by blefebvr          #+#    #+#             */
-/*   Updated: 2022/07/05 17:43:32 by blefebvr         ###   ########.fr       */
+/*   Updated: 2022/07/12 18:18:22 by blefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 // ssize_t read(int fd, void *buf, size_t nbyte);
 
 #include "get_next_line.h"
 
-void	free(void *ptr)
+static void	free(void *ptr)
 {
-	
+
 }
 
-char	*read_line(int fd)
+static char	*read_line(int fd)
 {
 	ssize_t	reader;
 	static char	*stash;
 	char	*buffer;
-	unsigned int	i;
+	size_t	i;
 
 	i = 0;
-	buffer = malloc(sizeof *buffer * (BUFFER_SIZE + 1));
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
 		return (NULL);
-
 	reader = read(fd, buffer, BUFFER_SIZE);
 	while (reader != 0)
 	{
@@ -37,41 +36,84 @@ char	*read_line(int fd)
 		i++; 
 		reader--;
 	}
+	stash[i] = '\0';
+	free(buffer);
 	return (stash);
 }
 
-char	*get_line(static char *stash)
+static char	*get_line(char *line, static char *stash)
 {
-	unsigned int	i;
-	char	*line;
+	size_t	i;
+	size_t	j;
+	char	*tmp;
 
 	i = 0;
-	while (stash[i] != '\n')
-	{
-		line[i] = stash[i];
-		i++;
-	}
+	j = 0;
+	if (!find_sep(stash))
+		line = join_line(line, stash);
 	return (line);
 }
 
-char	*keep_stash(static char *stash)
+static char	*get new_line(static char stash, char *line)
 {
-	char	*stash;
-	unsigned int	i;
+	size_t	i;
 
-	
+	i = 0;
+	while (stash[i] != '\0')
+	{
+		while (stash[i] != '\n')
+			i++;
+		tmp = malloc(sizeof(char) * i + 2;
+		while (j < i)
+			tmp[j++] = stash[j++];
+		tmp[j] = '\n';
+		tmp[i] = '\0';
+		line = join_line(line, tmp);
+		stash = get_remaining_stash(stash);
+		i += 1;
+	}
+	free(tmp);
+	return (line);
+}
+
+static char	*get_new_line(char *stash)
+{
+	size_t	size;
+	size_t	i;
+
+	size = ft_strlen(tmp);
+	stash = malloc(sizeof(char *) * size);
+	i = 0;
+	while (!find_sep(stash))
+	{
+		
+		stash[i++] = tmp[i++];
+	free(tmp);
 	return (stash);
 }
 
 char	*get_next_line(int fd)
 {
-	char	*next_line;
-	
+	char	*line;
+	char	*
 
-	return (next_line);
+	line = "\0";
+	stash = read_line(fd);
+	if (!find_sep(stash))
+	{
+		line = get_line(line, stash);
+		stash = get_clean(stash);
+	}
+	else
+	{
+		line = get_new_line(stash);
+		stash = get_remaining_stash;
+	}
+	if (
+	return (line);
 }
 
-int	main(int argc, char **argv)
+/*int	main(int argc, char **argv)
 {
 	int	fd;
 	char	*line;
@@ -86,4 +128,4 @@ int	main(int argc, char **argv)
 	}
 	fd = close(fd);
 	return (0);
-}
+}*/
---
+++ get_next_line_utils.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blefebvr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 16:18:39 by blefebvr          #+#    #+#             */
+/*   Updated: 2022/07/12 17:34:04 by blefebvr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/*size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (i);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+static char	*ft_strcat(const char *dest, const char *src)
+{
+	size_t	i;
+	size_t	j;
+	char	*d;
+	char	*s;
+
+	d = (char *)dest;
+	s = (char *)src;
+	i = ft_strlen(d);
+	j = 0;
+	while (src[j] != '\0')
+	{
+		d[i] = s[j];
+			j++;
+			i++;
+	}
+	d[i] = '\0';
+	return ((char *)dest);
+}
+
+static size_t	find_sep(char *stash)
+{
+	size_t	i;
+
+	i = 0;
+	while (stash[i])
+	{
+		if (stash[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+static char	*get_remaining_stash(char *stash, char sep)
+{
+	int	i;
+
+	i = 0;
+	if (find_sep(stash))
+	{
+		while (stash[i] != '\0')
+		{
+			if (stash[i] == sep)
+				return (&stash[i + 1]);
+			i++;
+		}
+	}
+	else if (!find_sep(stash))
+		return (stash);
+	return (0);
+}
+
+static char	*join_line(char *line, char *stash)
+{
+	unsigned int	size;
+	char		*joined;
+
+	size = (ft_strlen(line) + ft_strlen(stash));
+	if (size == 0)
+	{
+		joined = malloc(sizeof(char));
+		if (joined == NULL)
+			return (0);
+		joined[0] = '\0';
+		return (joined);
+	}
+	joined = malloc(sizeof(char) * size + 1);
+	if (joined == NULL)
+		return (0);
+	joined[0] = '\0';
+	ft_strcat(joined, line);
+	ft_strcat(joined, stash);
+	return (joined);
+}*/
+
+int main()
+{
+	//char s1[] = "Adam,";
+	char s2[] = "Salut \ntoi";
+	int i = 0;
+
+	while (s2[i] != '\n')
+		i++;
+	printf("%d\n", i);
+	i += 1;
+	while (s2[i] != '\0')
+		write(1, &s2[i++], 1);
+	return (0);
+}
